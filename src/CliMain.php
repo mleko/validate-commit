212fb30949510efe5c05e3575d859c7b81aabe6c
Daniel Król <daniel@krol.me>
2016-03-02 11:21:51 +0100

    Initial commit

---
+++ src/CliMain.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace Mleko\ValidateCommit;
+
+
+class CliMain
+{
+    /**
+     * @param string[] $argv
+     * @return boolean proceed(true) or abort(false) commit
+     */
+    public function run($argv)
+    {
+        return true;
+    }
+
+
+}
